72917d9044976e762f66b023f32f1ede64935de5
amandadumi <amanda.e.dumi@gmail.com>
2023-03-02 16:06:45 -0700

    bare file structure for snap jastrow, small test change

--- src/QMCWaveFunctions/Jastrow/SNAPJastrow.h
+++ src/QMCWaveFunctions/Jastrow/SNAPJastrow.h
@@ -1,59 +1,75 @@
 #ifndef QMCPLUSPLUS_SNAPJASTROW
 #define QMCPLUSPLUS_SNAPJASTROW
-#include "Configuration.h"
-#if !defined(QMC_BUILD_SANDBOX_ONLY)
 #include "QMCWaveFunctions/WaveFunctionComponent.h"
-#endif
 #include "Particle/DistanceTable.h"
-#include "CPU/SIMD/aligned_allocator.hpp"
-#include "CPU/SIMD/algorithm.hpp"
-#include <map>
-#include <numeric>
-#include <memory>
 
 
 
-std::string std::string getClassName() const override {return "SNAPJastrow";}
+namespace qmcplusplus
+{
+class SNAPJastrow : public WaveFunctionComponent
+{
 
-/** Accpted move. Update Vat[iat],Grad[iat] and Lap[iat] */
-void acceptMove(ParticleSet& P, int iat, bool safe_to_delay = false) override{
-    UpdateMode = ORB_PBYP_RATIO;
-    curAt      = computeU(P.getDistTableAB(myTableID).getTempDists());
+public:
 
-};
+    SNAPJastrow();
 
+    ~SNAPJastrow();
 
-inline void restore(int iat) override {}
+    std::string getClassName() const override {return "SNAPJastrow";}
 
-PsiValueType ratio(ParticleSet& P, int iat) overrride {
+    /** Initialize a lammps object to get bispectrom components from current particle set.
+    | * 
+    | */
+    void initialize_lammps();
 
-}
+/******MC step related functions******/
 
-void registerData(ParticleSet& P, WFBufferType& buf) override {}
-
-LogValueType updateBuffer(ParticleSet& P, WFBufferType& buf, bool fromscratch = false) override {}
-
-void copyFromBuffer(ParticleSet& P, WFBufferType& buf) override {}
-
-/** Initialize a lammps object to get bispectrom components from current particle set.
-| * 
-| */
-void initialize_lammps(){
-
-}
-
-/** From exsisting lammps object, get bispectrum components.
-| * 
-| */
-void access_bispectrum(){
+    /** Accpted move. Update Vat[iat],Grad[iat] and Lap[iat] */
+    void acceptMove(ParticleSet& P, int iat, bool safe_to_delay = false) override; 
+    inline void restore(int iat) override {}
+    /** From exsisting lammps object, get bispectrum components.
+    | * 
+    | */
+    void access_bispectrum();
 
 
-}
+/******Optimization related functions******/
+    bool isOptimizable() const override { return true; }
+    /** check out optimizable variables
+    */
+    void checkOutVariables(const opt_variables_type& o) override;
+
+    void extractOptimizableObjectRefs(UniqueOptObjRefs& opt_obj_refs) override;
+
+
+/****** Evaluate E_L functions ******/
+    PsiValueType ratio(ParticleSet& P, int iat) override;
+    GradType evalGrad(ParticleSet& P, int iat) override;
+    PsiValueType ratioGrad(ParticleSet& P, int iat, GradType& grad_iat) override;
+    LogValueType evaluateGL(const ParticleSet& P,
+                                  ParticleSet::ParticleGradient& G,
+                                  ParticleSet::ParticleLaplacian& L,
+                                  bool fromscratch);
+
+
+/******Checkout related functons******/
+    void registerData(ParticleSet& P, WFBufferType& buf) override;
+
+    LogValueType updateBuffer(ParticleSet& P, WFBufferType& buf, bool fromscratch = false) override;
+
+    void copyFromBuffer(ParticleSet& P, WFBufferType& buf) override;
+
+
 
 
 void evaluateDerivatives(ParticleSet& P,
                                    const opt_variables_type& optvars,
                                    Vector<ValueType>& dlogpsi,
-                                   Vector<ValueType>& dhpsioverpsi) override {
+                                   Vector<ValueType>& dhpsioverpsi) override;
 
-                                   }
+private:
+
+};
+}
+#endif
