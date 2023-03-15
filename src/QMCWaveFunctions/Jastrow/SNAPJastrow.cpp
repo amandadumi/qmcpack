cd59d310796de40e4c3e1b2afff210c909fbf8d9
amandadumi <amanda.e.dumi@gmail.com>
2023-03-15 17:34:14 -0600

    initialize particles

--- src/QMCWaveFunctions/Jastrow/SNAPJastrow.cpp
+++ src/QMCWaveFunctions/Jastrow/SNAPJastrow.cpp
@@ -16,18 +16,38 @@ SNAPJastorw::initialize_lammps(ParticleSet& P){
     lmp->input->one("neigh_modify every 1 delay 1 check yes ");
     //TODO: what will box region be pased on QMC object. Cell?
     lmp->input->one("region	mybox block -50 50 -50 50 -50 50");
+    // create a box that will contain the number of species equal to the number of groups.
+    std::string temp_command = "create_box" + P.groups() +  "mybox";
+    lmp->input->one(temp_command);
+    lmp->input->one("group all type 1 2");
+    // add atoms to the groups
+    for (int ig = 0; ig < P.groups(); ig++) { // loop over groups
+        // label groups in lamps
+        lmp->input->one("group electrons type 2");
+    f or (int iat = P.first(ig); iat < P.last(ig); iat++) { // loop over elements in each group
+    // place atom in boxes according to their group.
+        temp_command = "create_atoms" + std::string(ig)+1 + "single " + std::to_string(P.R[iat][0]) + "  " +std::to_string(P.R[iat][1])  + " " + std::to_string(P.R[iat][2]);  
+         lmp->input->one(temp_command);
 
-
-    
-    int n_particle_types = 0; // not sure how to access this yet. // this will be through distance table!
-    for (int i =0; i <n_particle_types){
-        n_this_particle_type = i.getTotalNum()
+     }
     }
+            // create atoms in a certain group of particles with certain coordinates.
 
+        }
+    }
+    lmp->input->one("reset_timestep 0");
+    lmp->input->one("group all type 1 2");
+    lmp->input->one("include WBe_Wood_PRB2019.snap");
+    lmp->input->one("velocity       all create 298.15 4928459 rot yes mom yes dist gaussian");
+    lmp->input->one("fix            ensemble all nve temp 0 298.15 100 tchain 1");
+    lmp->input->one("timestep       .01");
+
+    lmp->input->one("thermo_style   one");
+    lmp->input->one("thermo         50");
     // loop over particle types(ions,alpha_es, beta_es?)
 
     // take position of first particle, create lammps container, put positions of all particles of that type in container
-    P.R;
+    
     // use particle set to initialize a lammps object with bispectrum randomly initialized.
 }
 SNAPJastorw::access_bispectrum(){
