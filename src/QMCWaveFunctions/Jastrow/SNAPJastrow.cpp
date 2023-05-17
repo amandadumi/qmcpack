3ec3df688af50adeb243fb7e83f94a8f763ed2bc
amandadumi <amanda.e.dumi@gmail.com>
2023-05-17 15:40:09 -0600

    gradlogpsi and laplogpsi finite difference

--- src/QMCWaveFunctions/Jastrow/SNAPJastrow.cpp
+++ src/QMCWaveFunctions/Jastrow/SNAPJastrow.cpp
@@ -4,12 +4,16 @@
 namespace qmcplusplus
 {
 
-SNAPJastrow::SNAPJastrow(const ParticleSet& ions, ParticleSet& els){
-    Nelec = els.getTotalNum();
-    Nions = ions.getTotalNum();
-    NIonGroups = ions.groups() ;
+SNAPJastrow::SNAPJastrow(const std::string& obj_name,const ParticleSet& ions, ParticleSet& els) 
+  : WaveFunctionComponent(obj_name),
+    myTableID(els.addTable(ions)),
+    Nelec(els.getTotalNum()),
+    Nions(ions.getTotalNum()),
+    NIonGroups(ions.groups()),
+    Ions(ions)
+{
     std::string iSpecies, eSpecies1, eSpecies2;
-    double ncoeff = 56;
+    ncoeff = 56;
     std::vector<double> default_coeff; // this should be a matrix where we resize to size of Nspecies*ncoeff
 }
 
@@ -76,7 +80,7 @@ SNAPJastrow::LogValueType SNAPJastrow::evaluateGL(const ParticleSet& P,
             // loop over elecs in each group
             for (int dim = 0; dim < OHMMS_DIM; dim++){
                 //G[iel][dim] = lammps_object
-                int ncols = nbispectrum+1
+                int ncols = ncoeff+1;
                 G[iel][dim] += static_cast<double>(*sna_global->array[(iel*3*ncols)+(dim*ncols)+ncols]);
                 //forward direction
                 RealType r0 = P.R[iel][dim];
@@ -88,16 +92,18 @@ SNAPJastrow::LogValueType SNAPJastrow::evaluateGL(const ParticleSet& P,
                 G_finite_diff_forward = static_cast<double>(*sna_global->array[(iel*3*ncols)+(dim*ncols)+ncols]);
                 //gradient
                 //backward direction
-                r0 = P.R[iel][dim];
                 RealType rm   = r0 - (delta/2);
                 //update lammps position
                 x[iel+dim] = rm;
                 sna_global -> compute_peratom();
-                G_finite_diff_back = static_cast<double>(*sna_global->array[(eil*3*ncols)+(dim*ncols)+(ncols)]);
+                G_finite_diff_back = static_cast<double>(*sna_global->array[(iel*3*ncols)+(dim*ncols)+(ncols)]);
                 // recalculate bispectrum stuff
                 double finite_diff_lap = (G_finite_diff_forward - G_finite_diff_back)/delta;
                 //fill L
                 L[iel] -=  finite_diff_lap;
+                // return coordinates to original
+                x[iel+dim] = r0;
+
               }
         }
     }
@@ -105,9 +111,8 @@ SNAPJastrow::LogValueType SNAPJastrow::evaluateGL(const ParticleSet& P,
    return log_value_;
 }
 
-void SNAPJastrow::lammps_evaluateGL(){
 
-}
+
 
 SNAPJastrow::LogValueType SNAPJastrow::evaluateLog(const ParticleSet& P,
                                   ParticleSet::ParticleGradient& G,
@@ -118,7 +123,7 @@ SNAPJastrow::LogValueType SNAPJastrow::evaluateLog(const ParticleSet& P,
     //         double** coeffs = static_cast<double**>lmp->force->pair->coeffelem;
     //     }
         lmp->input->one("run            0");
-        log_value_ =  static_cast<double>lmp->force->pair->eng_vdwl;
+        log_value_ =  static_cast<double>(lmp->force->pair->eng_vdwl);
 
     // loop over bispectrum compoenents
     // calculate linear energy 
@@ -128,7 +133,7 @@ SNAPJastrow::LogValueType SNAPJastrow::evaluateLog(const ParticleSet& P,
 
 void SNAPJastrow::evaluateDerivatives(ParticleSet& P, const opt_variables_type& optvars, Vector<ValueType>& dlogpsi, Vector<ValueType>& dhpsioverpsi)
 {
-    evaluateDerivativesWF(P, active, dlogpsi);
+    evaluateDerivativesWF(P, optvars, dlogpsi);
     bool recalculate(false);
     std::vector<bool> rcsingles(myVars.size(), false);
     for (int k = 0; k < myVars.size(); ++k)
@@ -136,7 +141,7 @@ void SNAPJastrow::evaluateDerivatives(ParticleSet& P, const opt_variables_type& 
       int kk = myVars.where(k);
       if (kk < 0)
         continue;
-      if (active.recompute(kk))
+      if (optvars.recompute(kk))
         recalculate = true;
       rcsingles[k] = true;
     }
@@ -167,7 +172,7 @@ for (int k = 0; k < myVars.size(); ++k)
       int kk = myVars.where(k);
       if (kk < 0)
         continue;
-      if (active.recompute(kk))
+      if (optvars.recompute(kk))
         recalculate = true;
       rcsingles[k] = true;
     }
@@ -177,8 +182,7 @@ for (int k = 0; k < myVars.size(); ++k)
             gradLogPsi[p] = 0.0;
             lapLogPsi[p] = 0.0;
         }
-    const auto& d_table: const auto & = P.getDistTableAB(myTableID);
-    std::vector<TinyVector<RealType,3>> derivs(NumVars);
+    const auto& d_table = P.getDistTableAB(myTableID);
 
     constexpr RealType cone(1);
     const size_t ns = d_table.sources();
@@ -186,7 +190,6 @@ for (int k = 0; k < myVars.size(); ++k)
     
     std::vector<PosType> displ(nt);
 
-    std::fill(first: derivs.begin(), last:derivs.end(),value: 0);
         /*
         $\partial E_{SNAP}/\partial c$
         1. have snap energy with current coefficients at current positions. 
@@ -203,24 +206,131 @@ for (int k = 0; k < myVars.size(); ++k)
     RealType fd_u;
     RealType bd_u;
     RealType delta = 0.1;
-    for (nv = 0; nv < NumVars; nv++){
-    fd_coeff[nv] = lmp->force->pair->snap->beta[nv] + delta;
-    bd_coeff[nv] = lmp->force->pair->snap->beta[nv] - delta;
-    calculate_internal_ESNAP_CD(fd_coeff, fd_u);
-    calculate_internal_ESNAP_CD(bd_coeff, bd_u);
-    dlogpsi = fd_u - bd_u/(2*delta);
-    dLogPsi[p] -= dlogpsi;
+    for (int nv = 0; nv < NumVars; nv++){
+      fd_coeff[nv] = lmp->force->pair->snap->beta[nv] + delta;
+      bd_coeff[nv] = lmp->force->pair->snap->beta[nv] - delta;
+      calculate_internal_ESNAP_CD(fd_coeff, fd_u);
+      calculate_internal_ESNAP_CD(bd_coeff, bd_u);
+      dlogpsi = fd_u - bd_u/(2*delta);
+      dLogPsi[nv] -= dlogpsi;
     }
-
+    calculate_ddc_gradlap_lammps(delta,fd_coeff,bd_coeff);
     }
-     
 }
+
+
+    void SNAPJastrow::calculate_ddc_gradlap_lammps(RealType delta, std::vector<RealType> fd_coeff, std::vector<RealType> bd_coeff){
+       /** brute force calculation of grad lammp where lammps object will have coefficients updated and reran.*/ 
+       // 1. update coeffs to forward direction
+       std::vector<SNAPJastrow::GradDerivVec> grad_forward;
+       std::vector<SNAPJastrow::GradDerivVec> grad_back;
+       std::vector<SNAPJastrow::GradDerivVec> grad_cd;
+
+       std::vector<SNAPJastrow::ValueDerivVec> lap_forward;
+       std::vector<SNAPJastrow::ValueDerivVec> lap_back;
+       std::vector<SNAPJastrow::ValueDerivVec> lap_cd;
+       std::vector<RealType> lap_back;
+
+       coeff_pointer = lammps->coeffs;
+       std::vector<std::vector<RealType,3>> true_coeffs;
+
+       for (i=0;i<fd_coeff;i++){
+       true_coeffs[i] = (RealType)*lammps->coeffs[i] // store number not pointer
+       // 1. update coeffs to forward direction
+       coeff_pointer[i] = fd_coeff[i]
+       // 2. run lammps command to update global array
+       lmp->input->one("run            0");
+       // 3. calculate grad and lap
+       int nelec = P.getTotalNum();
+       for (int ig = 0; ig < P.groups(); ig++) {
+          for (int iel = P.first(ig); iel < P.last(ig); iel++){ 
+            for (int dim = 0; dim < OHMMS_DIM; dim++){
+             //G[iel][dim] = lammps_object
+             int ncols = ncoeff+1
+             grad_forward[iel][dim] += static_cast<double>(*sna_global->array[(iel*3*ncols)+(dim*ncols)+ncols]);
+             // calc lap d/dr grad
+             //forward direction
+             RealType r0 = P.R[iel][dim];
+             RealType rp   = r0 + (delta/2);
+             //update lammps position
+             x[iel+dim] = rp;
+             // recalculate bispectrum stuff
+             sna_global -> compute_peratom();
+             G_finite_diff_forward = static_cast<double>(*sna_global->array[(iel*3*ncols)+(dim*ncols)+ncols]);
+             //back direction
+             RealType rm   = r0 - (delta/2);
+             //update lammps position
+             x[iel+dim] = rp;
+             // recalculate bispectrum stuff
+             sna_global -> compute_peratom();
+             G_finite_diff_back = static_cast<double>(*sna_global->array[(iel*3*ncols)+(dim*ncols)+ncols]);
+             double finite_diff_lap = (G_finite_diff_forward - G_finite_diff_back)/delta;
+             //fill L
+             lap_forward[iel] -=  finite_diff_lap;
+             // return coordinates to original
+             x[iel+dim] = r0;
+            }
+          }
+       }
+       // 4. update coeffs to backwards direction 
+       coeff_pointer[i] = bd_coeff[i]
+       // 5. run lammps command to update global array
+       lmp->input->one("run            0");
+       // 6. calculate grad and lap
+       for (int ig = 0; ig < P.groups(); ig++) {
+          for (int iel = P.first(ig); iel < P.last(ig); iel++){ 
+            for (int dim = 0; dim < OHMMS_DIM; dim++){
+             //G[iel][dim] = lammps_object
+             int ncols = ncoeff+1
+             grad_backward[iel][dim] += static_cast<double>(*sna_global->array[(iel*3*ncols)+(dim*ncols)+ncols]);
+             // calc lap d/dr grad
+             //forward direction
+             RealType r0 = P.R[iel][dim];
+             RealType rp   = r0 + (delta/2);
+             //update lammps position
+             x[iel+dim] = rp;
+             // recalculate bispectrum stuff
+             sna_global -> compute_peratom();
+             G_finite_diff_forward = static_cast<double>(*sna_global->array[(iel*3*ncols)+(dim*ncols)+ncols]);
+             //back direction
+             RealType rm   = r0 - (delta/2);
+             //update lammps position
+             x[iel+dim] = rp;
+             // recalculate bispectrum stuff
+             sna_global -> compute_peratom();
+             G_finite_diff_back = static_cast<double>(*sna_global->array[(iel*3*ncols)+(dim*ncols)+ncols]);
+             double finite_diff_lap = (G_finite_diff_forward - G_finite_diff_back)/delta;
+             //fill L
+             lap_back[iel] -=  finite_diff_lap;
+             // return coordinates to original
+             x[iel+dim] = r0;
+             }
+          lap_cd = (lap_forward-lap_back)/2*delta;
+          }
+       }
+       // 7. use central difference to get estimate of ddc grad and lap  which populates gradLogPsi and lapLogPsi
+       // centra diff grad
+       //AD: can probably combine this and above loop. 
+       for (int ig = 0; ig < P.groups(); ig++) {
+          for (int iel = P.first(ig); iel < P.last(ig); iel++){ 
+            for (int dim = 0; dim < OHMMS_DIM; dim++){
+              grad_cd[iel][dim] = (grad_forward[iel][dim] - grad_background[iel][dim])/del;
+            }
+            lap_cd[iel] = (lap_forward[iel]-lap_back[iel])/2*delta;
+          }
+     
+        }
+      }
+    gradLogPsi = grad_cd;
+    lapLogPsi  = lap_cd;
+  
+  }
 
 /* calculates esnap based on a set of coefficients manually in qmcpack
 used to see impact of small change in coefficients on snap energy (needed to calculated d E/d beta)
 without having to internally change the lammps object.
 */
-void calculate_internal_ESNAP_CD(std::vector<RealType> new_coeff, new_u){
+void calculate_internal_ESNAP_CD(std::vector<RealType> new_coeff, RealType new_u){
   RealType ESNAP_all = 0;
   RealType ESNAP_elec;
   RealType ESNAP_ion;
@@ -240,12 +350,12 @@ void calculate_internal_ESNAP_CD(std::vector<RealType> new_coeff, new_u){
     ESNAP_all += ESNAP_i;
     }
   }
-  bispectrum_block = P.groups() // number of groups for electrons will be block for ions. 2*nbispectrum = start of ions.
+  bispectrum_block = P.groups() // number of groups for electrons will be block for ions. 2*ncoeff = start of ions.
   //TODO: only written for single ion at the moment.
-  for (s = 0; s <ns; s++){
+  for (int s = 0; s <ns; s++){
     ESNAP_i = 0;
       for (k=0; k < ncoeff; k++){
-        RealType bispectrum_val = sna_global[bispectrum_block*n_bispectrum+k];
+        RealType bispectrum_val = sna_global[bispectrum_block*ncoeff+k];
         ESNAP_i += beta[bispectrum_block][k] * bispectrum_val  ;
       }
     ESNAP_all += ESNAP_i;
